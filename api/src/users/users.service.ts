4ef52479510dc9684419d900f661752afb645327
Barathi <barathiofficial@gmail.com>
2024-04-07 22:12:59 +0530

    feat: add activate service

--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,7 +1,12 @@
-import { Injectable } from '@nestjs/common'
+import {
+	BadRequestException,
+	Injectable,
+	NotFoundException
+} from '@nestjs/common'
 import { DatabaseService } from 'src/database/database.service'
 import { MailService } from 'src/mail/mail.service'
 import { OtpService } from 'src/otp/otp.service'
+import { ActivateUserDto, UpdateUserDto } from './dto'
 import { CreateUserDto } from './dto/create-user.dto'
 
 @Injectable()
@@ -19,6 +24,29 @@ export class UsersService {
 		this.mailService.sendOtp(user.email, otp)
 
 		return user
+	}
+
+	async activate(data: ActivateUserDto) {
+		const user = await this.findUnique(data.email)
+
+		if (!user) {
+			throw new NotFoundException('User not found')
+		}
+
+		const otp = await this.otpService.findOtp(user.id, data.otp)
+
+		if (!otp) {
+			throw new BadRequestException('Invalid OTP')
+		}
+
+		return this.update({ verified: true }, user.id)
+	}
+
+	update(data: UpdateUserDto, id: string) {
+		return this.db.user.update({
+			data,
+			where: { id }
+		})
 	}
 
 	findUnique(email: string) {
