23f05206eb4d9a23899449c96e0219940f4f5623
Barathi <barathiofficial@gmail.com>
2024-04-07 22:13:43 +0530

    feat: add update user dto

--- api/src/users/dto/update-user.dto.ts
+++ api/src/users/dto/update-user.dto.ts
@@ -1,4 +1,16 @@
 import { PartialType } from '@nestjs/mapped-types'
-import { CreateUserDto } from './create-user.dto'
+import { IsBoolean, IsNotEmpty, IsString, Length } from 'class-validator'
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+class UpdateUser {
+	@IsBoolean({ message: 'Verification status must be a boolean' })
+	verified: boolean
+
+	@IsString({ message: 'Name must be a string' })
+	@IsNotEmpty({ message: 'Name is required' })
+	@Length(1, 30, {
+		message: 'Name length must be between 1 and 30 characters'
+	})
+	fullname: string
+}
+
+export class UpdateUserDto extends PartialType(UpdateUser) {}
