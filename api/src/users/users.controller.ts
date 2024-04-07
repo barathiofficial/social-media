170deea1393810bf42a18a52830053aa4602c982
Barathi <barathiofficial@gmail.com>
2024-04-07 21:27:46 +0530

    fix: move logic from service to controller

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -1,13 +1,45 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common'
+import {
+	Body,
+	ConflictException,
+	Controller,
+	Post,
+	ValidationPipe
+} from '@nestjs/common'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
+import { MailService } from 'src/mail/mail.service'
+import { CreateOtpDto } from 'src/otp/dto'
+import { OtpService } from 'src/otp/otp.service'
 import { CreateUserDto } from './dto'
 import { UsersService } from './users.service'
 
 @Controller('users')
 export class UsersController {
-	constructor(private readonly usersService: UsersService) {}
+	constructor(
+		private readonly usersService: UsersService,
+		private readonly otpService: OtpService,
+		private readonly mailService: MailService
+	) {}
 
 	@Post()
-	create(@Body(ValidationPipe) data: CreateUserDto) {
-		return this.usersService.create(data)
+	async create(@Body(ValidationPipe) data: CreateUserDto) {
+		try {
+			const user = await this.usersService.create(data)
+			const otp = this.otpService.generateOtp()
+			const createOtpDto: CreateOtpDto = { otp, userId: user.id }
+
+			await this.otpService.create(createOtpDto)
+
+			this.mailService.sendOtp(data.email, otp)
+
+			return user
+		} catch (error) {
+			if (error instanceof PrismaClientKnownRequestError) {
+				if (error.code === 'P2002') {
+					throw new ConflictException('Email already exist')
+				}
+			}
+
+			throw new Error(error)
+		}
 	}
 }
--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,35 +1,13 @@
-import { ConflictException, Injectable } from '@nestjs/common'
+import { Injectable } from '@nestjs/common'
 import { DatabaseService } from 'src/database/database.service'
-import { MailService } from 'src/mail/mail.service'
-import { CreateOtpDto } from 'src/otp/dto'
-import { OtpService } from 'src/otp/otp.service'
 import { CreateUserDto } from './dto/create-user.dto'
 
 @Injectable()
 export class UsersService {
-	constructor(
-		private readonly db: DatabaseService,
-		private readonly otpService: OtpService,
-		private readonly mailService: MailService
-	) {}
+	constructor(private readonly db: DatabaseService) {}
 
-	async create(data: CreateUserDto) {
-		const user = await this.findUnique(data.email)
-
-		if (user) {
-			throw new ConflictException('Email already exist')
-		}
-
-		const newUser = await this.db.user.create({ data })
-		const otp = this.otpService.generateOtp()
-		const userId = newUser.id
-		const createOtpDto: CreateOtpDto = { otp, userId }
-
-		await this.otpService.create(createOtpDto)
-
-		this.mailService.sendOtp(data.email, otp)
-
-		return newUser
+	create(data: CreateUserDto) {
+		return this.db.user.create({ data })
 	}
 
 	findUnique(email: string) {
