2de56426569dc7c25759779015c98b023ace1501
Barathi <barathiofficial@gmail.com>
2024-04-09 23:08:07 +0530

    import jwtConfig to appModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module'
-import { dbConfig, mailConfig } from './config'
+import { dbConfig, jwtConfig, mailConfig } from './config'
 import { DatabaseModule } from './database/database.module'
 import { MailModule } from './mail/mail.module'
 import { OtpModule } from './otp/otp.module'
@@ -10,7 +10,7 @@ import { UsersModule } from './users/users.module'
 @Module({
 	imports: [
 		ConfigModule.forRoot({
-			load: [mailConfig, dbConfig]
+			load: [mailConfig, dbConfig, jwtConfig]
 		}),
 		UsersModule,
 		DatabaseModule,
